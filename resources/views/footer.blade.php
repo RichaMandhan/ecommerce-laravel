715a20a07fc773b2a681f6a4ff15e2135af17038
Richa <richamandhan65@gmail.com>
2021-06-26 22:12:33 +0530

    header, footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="panel panel-default">
+ <!-- <div class="panel-body">
+    Panel content
+  </div> -->
+  <div class="panel-footer">Copyright @<?php echo date('Y'); ?> all rights reserved.</div>
+</div>
